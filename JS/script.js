19e4c263c431e350acf6fc9e95db06246b141ba1
lubaghonim <luba.ghonim@gmail.com>
2023-10-10 14:02:09 +0700

    HAMPIR RILIS

--- JS/script.js
+++ JS/script.js
@@ -10,29 +10,30 @@ let availableKeywords = [
     'Human Resources Development',
     'Career&Development',
 ]
-const search = document.getElementById("search");
-const results = document.querySelector(".results");
 
-search.onkeyup = function(){
-    let result = [];
-    let input = search.value;
-    if(input.length){
-      result = availableKeywords.filter((keyword) => {
-        return keywordtoLowerCase().includes(input.toLowerCase());
-      });
-      console.log(result);
-    }
-    display(result);
+// const search = document.getElementById("search");
+// const results = document.querySelector(".results");
+    
+// search.onkeyup = function(){
+//     let result = [];
+//     let input = search.value;
+//     if(input.length){
+//       result = availableKeywords.filter((keyword) => {
+//         return keyword.toLowerCase().includes(input.toLowerCase());
+//       });
+//       console.log(result);
+//     }
+//     display(result);
 
-    if(!result.length){
-        results.innerHTML = "";
-    }
-}
+//     if(!result.length){
+//         results.innerHTML = "";
+//     }
+// }
   
 function display(result){
-    const content = result.map(list)=>{
-        return "<li onclick=selectInput(this)>" + list + <"li">;
-    });
+    const content = result.map((list) => {
+        return "<li onclick=selectInput(this)>" + list + "</li>";
+    });    
 
     results.innerHTML = "<ul>" + content.join("") + "<ul>";
 }
@@ -43,63 +44,94 @@ function selectInput(list){
 }
 
 //contact us
-const nameInput = document.querySelector("#yourName");
-const email = document.querySelector("#emailAddress");
-const subject = document.querySelector("#subject");
-const message = document.querySelector("#yourMessage");
-const succes = document.querySelector("#succes");
+const nameInput = document.getElementById("yourName");
+const email = document.getElementById("emailAddress");
+const subject = document.getElementById("subject");
+const message = document.getElementById("yourMessage");
+const succes = document.getElementById("succes");
 const errorNodes = document.querySelectorAll(".error");
+const form = document.querySelector('#formContact')
 
-const form = document.querySelector("form"); // Anda perlu menambahkan selektor yang sesuai untuk form Anda
-
-form.addEventListener('submit', (e) => {
+// kita berikan event submit pada form nya
+form.addEventListener('submit', function(e){
     e.preventDefault();
-    validateForm();
-});
 
-function validateForm(){
-
-    clearMessages();
-    let errorFlag = false;
-
-    if(nameInput.value.length<1){
-        errorNodes[0].innerText="Name cannot be blank";
-        nameInput.classList.add("error-border")
-        errorFlag = true;
-    }
-    if(!emailIsValid(email.value)){
-        errorNodes[1].innerText="Invalid email address";
-        email.classList.add("error-border")
-        errorFlag = true;
-    }
-    if(subject.value.length<1){
-        errorNodes[2].innerText="Please enter a subject";
-        subject.classList.add("error-border")
-        errorFlag = true;
-    }
-    if(message.value.length<1){
-        errorNodes[3].innerText="Please enter message";
-        email.classList.add("error-border")
-        errorFlag = true;
-    }
-    if(!errorFlag){
-        succes.innerText ="";
-    }
-}
-
-function clearMessages(){
-    for(i=0, i < errorNodes.length; i++){
-        errorNodes[i].innerText="";
+    const data = {
+        names: nameInput.value,
+        mail:email.value,
+        subjek: subject.value,
+        pesan:message.value,
     }
 
-    succes.innerText=""
-    nameInput.classList.remove("error-border");
-    email.classList.remove("error-border");
-    subject.classList.remove("error-border");
-    message.classList.remove("error-border");
-}
+    fetch('https://be-balikpapan-16-production.up.railway.app/contact', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert("berhasil mengirimkan data")
 
-function emailIsValid(email){
-    let pattern = /\S+$\S+\.\S+/;
-    return pattern.test(email);
-}
+        // clear semua inputan form
+        form.reset()
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+    console.log(data)
+
+
+
+    // alert(200);
+})
+
+
+// function validateForm(){
+
+//     clearMessages();
+//     let errorFlag = false;
+
+//     if(nameInput.value.length<1){
+//         errorNodes[0].innerText="Name cannot be blank";
+//         nameInput.classList.add("error-border");
+//         errorFlag = true;
+//     }
+//     if(!emailIsValid(email.value)){
+//         errorNodes[1].innerText="Invalid email address";
+//         email.classList.add("error-border");
+//         errorFlag = true;
+//     }
+//     if(subject.value.length<1){
+//         errorNodes[2].innerText="Please enter a subject";
+//         subject.classList.add("error-border");
+//         errorFlag = true;
+//     }
+//     if(message.value.length<1){
+//         errorNodes[3].innerText="Please enter message";
+//         message.classList.add("error-border");
+//         errorFlag = true;
+//     }
+//     if(!errorFlag){
+//         succes.innerText ="";
+//     }
+// }
+
+// function clearMessages(){
+//     for(let i=0; i < errorNodes.length; i++){
+//         errorNodes[i].innerText="";
+//     }
+
+//     succes.innerText="";
+//     nameInput.classList.remove("error-border");
+//     email.classList.remove("error-border");
+//     subject.classList.remove("error-border");
+//     message.classList.remove("error-border");
+// }
+
+// function emailIsValid(email){
+//     let pattern = /\S+$\S+\.\S+/;
+//     return pattern.test(email);
+// }
